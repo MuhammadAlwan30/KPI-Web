652474bd9384a4e6fed401e78245241a2abeb36f
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-02-10 09:30:27 +0700

    hapus sebagian

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,6 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
