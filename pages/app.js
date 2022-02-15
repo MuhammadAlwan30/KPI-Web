82f57c38dc3890f3953d15336eff55dfda0f2f68
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-02-15 17:54:43 +0700

    create app.js

---
+++ pages/app.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+function test(){
+ return <>
+   <Head>
+     <title>Test my app</title>
+   </Head>
+     <div className="text-center font-sans text-500-slate">
+      Wellcome to KPI
+     </div>
+   </>
+}
