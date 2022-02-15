521d66085aaeb49efa708279679f01524a2955d7
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-02-15 18:15:27 +0700

    astaghfirullah

--- pages/app.js
+++ pages/app.js
@@ -6,12 +6,13 @@ export default function app(){
    <Head>
      <title>Test my app</title>
      <meta name="description" content="selamat datang di komunitas programming Indonesia" />
+     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
    </Head>
      <div className="text-center font-sans text-500-slate bg-blue-500 px-3 py-3 w-auto flex items-center">
       Wellcome to KPI
      </div>
       <div className="w-16 h-16 bg-sky-500 flex items-center justify-center">
-        <div className="text-center font-sans block>
+        <div className="text-center font-sans block">
            Home
         </div>
      </div>
