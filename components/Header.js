f959c9efb229a616b8e3cae49d4bf7f380e8eeba
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-03-22 14:41:46 +0700

    change name route

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,10 @@ import react from "react";
     <header className="bg-violet-300 flex justify-center items-center flex-row w-full h-20">
     <nav className="flex-row">
       <ul className="inline-flex space-x-4">
-            <li className="hover:bg-slate-200 hover:underline"><a href="#">dev</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">ut</a></li>
-            <li><a href="#">ui</a></li>
-            <li><a href="#">about</a></li>
+            <li><a href="/about">Job</a></li>
+            <li><a href="#">Discussion</a></li>
+            <li><a href="#">Thread</a></li>
+            <li><a href="#">Showcase</a></li>
       </ul>
    </nav>
   </header>
