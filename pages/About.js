b19c3192a2c6cab894a071ceeab51f3787ebedf6
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-03-22 12:39:04 +0700

    create about.js

---
+++ pages/About.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Footer from '../components/footer'
+
+export default function About(){
+    return(
+        <div>
+            <Footer />
+        </div>
+    )
+}
