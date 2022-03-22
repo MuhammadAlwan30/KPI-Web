88cf018025377b2a42c3323b1c5841d70c782a80
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-03-22 14:24:27 +0700

    try props

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Footer from '../components/Footer';
 
+const description ="halo selamat datang di komunitas programming indonesia"
+
 export default function app(){
  return <>
    <Head>
-     <title>Test my app</title>
-     <meta name="description" content="selamat datang di komunitas programming Indonesia" />
+     <title>This is your Home</title>
+     <meta name="description" content={description} />
      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
    </Head>
      <div className="text-center font-sans text-500-slate bg-blue-500 px-3 py-3 w-auto flex items-center">
